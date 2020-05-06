3e2f88b41e02cb58c542e0d7bbeb0f0f7f267982
Cesarion <elinov.aleksey@icloud.com>
2020-05-06 16:17:42 +0300

    Django_learn_ex2

---
+++ run.bat
@@ -0,0 +1,2 @@
+chcp 1251
+cmd /k "cd /d C:\Users\kxm-a\Desktop\Обучение GeekBrains\Django\venv\Scripts\ & activate & cd /d C:\Users\kxm-a\Desktop\Обучение GeekBrains\Django\ & chcp 866 & python manage.py runserver"
